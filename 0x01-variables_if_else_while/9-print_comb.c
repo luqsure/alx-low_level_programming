2829edeefd157900fa7af5b561b6ece3a7ba9249
ola <luqsure@gmail.com>
2023-03-17 17:07:41 -0700

    Patience, persistence and perspiration make an unbreatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ;  i < 10 ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
+
