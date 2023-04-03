a5f15dc0081884462d440c120c7b241ba5ea9d22
ola <luqsure@gmail.com>
2023-04-03 13:56:47 -0700

    Mempcy

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,18 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *| = haystack;
+		char * | = haystack;
 		char *p = needle;
 
-		while (*| == *p && *p != '\0')
+		while (*'|' == *p && *p != '\0')
 		{
-			|++;
+			'|'++;
 			p++;
 		}
+
 		if (*p == '\0')
 			return (haystack);
 	}
+
 	return (0);
 }
