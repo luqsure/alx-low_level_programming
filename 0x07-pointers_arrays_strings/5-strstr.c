ef3ecbc3b818a8d54d997674b605f7ea66df2f6a
ola <luqsure@gmail.com>
2023-04-03 12:53:08 -0700

    Memset

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *| = haystack;
+		char *p = needle;
+
+		while (*| == *p && *p != '\0')
+		{
+			|++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
